d3474f7a9d4f6ca707960fc6755a1e6fcc7e72a6
MarkSuttirak <suttirak.ch@zaviago.com>
2023-09-29 16:28:35 +0700

    Commit Codes

--- linkpage/vite.config.js
+++ linkpage/vite.config.js
@@ -5,13 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 8080,
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src')
 		}
 	},
 	build: {
-		outDir: '../linkpage',
+		outDir: '../linkter/public/linkpage',
 		emptyOutDir: true,
 		target: 'es2015',
 	},
