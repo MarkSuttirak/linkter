17e11ccf61370490a36f54f04afedc9375cf6519
MarkSuttirak <suttirak.ch@zaviago.com>
2023-09-29 14:19:12 +0700

    Update main.jsx

--- linkpage/src/main.jsx
+++ linkpage/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'react/jsx-runtime.js'
+import 'jsx-runtime'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
