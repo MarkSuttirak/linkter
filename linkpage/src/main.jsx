942e3d097b95a7cf9e4d1c7b57e8abb9aa52b67d
MarkSuttirak <suttirak.ch@zaviago.com>
2023-10-02 10:57:23 +0700

    Commit Codes

--- linkpage/src/main.jsx
+++ linkpage/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-import 'react/jsx-runtime.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
