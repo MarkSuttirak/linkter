6a5f034d0979b559916f1fd5d5456cb7b091508f
MarkSuttirak <suttirak.ch@zaviago.com>
2023-10-02 10:42:39 +0700

    Update main.jsx

--- linkpage/src/main.jsx
+++ linkpage/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-import 'jsx-runtime'
+import 'react/jsx-runtime.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
