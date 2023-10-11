4dee1403813f3d28863973122bd68c5140d938ce
AnatholyB1 <93599884+AnatholyB1@users.noreply.github.com>
2023-10-11 18:49:55 +0700

    v

--- linkpage/src/pages/setup.jsx
+++ linkpage/src/pages/setup.jsx
@@ -101,7 +101,7 @@ const Setup = () => {
       setToken(token)
       if(!reloaded){
         Cookies.set('reloaded', 'true')
-        window.location.reload(true);
+        navigate('/setup')
       }
     }else{
       line()
